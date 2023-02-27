bcf1c101ca6ca15fc8a97d8fe4c2d671f9234610
petercmh01 <minghaipeterchan@outlook.com>
2023-02-27 18:23:34 +0000

    Update 2 files
    - /modules/super_res_module.py
    - /modules/menu.py

--- modules/menu.py
+++ modules/menu.py
@@ -14,6 +14,7 @@ from utils.gui_utils import imgui_utils
 from modules.pca_module import PCA_Module
 from modules.training_module import TrainingModule
 from modules.compress_module import CompressModule
+from modules.super_res_module import SuperResModule
 from modules.autolume_live import States
 
 #----------------------------------------------------------------------------
@@ -23,34 +24,42 @@ class Menu:
         self.pca = PCA_Module(self)
         self.training = TrainingModule(self)
         self.compress = CompressModule(self)
+        self.super_res = SuperResModule(self)
 
     def __call__(self):
 
         # Make train and compress buttons call function as subprocess to avoid blocking
         # Begin control pane.
         imgui.set_next_window_position(0, 0)
-        imgui.set_next_window_size(self.app.content_width//3, (self.app.content_height * 3) //4)
+        imgui.set_next_window_size(self.app.content_width//4, (self.app.content_height * 4) //5)
         imgui.begin('Train##Menu', closable=False, flags=(imgui.WINDOW_NO_RESIZE | imgui.WINDOW_NO_MOVE))
         imgui.text("Train")
         self.training()
         imgui.end()
 
-        imgui.set_next_window_position(self.app.content_width // 3, 0)
-        imgui.set_next_window_size(self.app.content_width // 3, (self.app.content_height * 3) // 4)
+        imgui.set_next_window_position(self.app.content_width // 4, 0)
+        imgui.set_next_window_size(self.app.content_width // 4, (self.app.content_height * 4) // 5)
         imgui.begin('Compress##Menu', closable=False, flags=(imgui.WINDOW_NO_RESIZE | imgui.WINDOW_NO_MOVE))
         imgui.text("Compress")
         self.compress()
         imgui.end()
 
-        imgui.set_next_window_position((2 * self.app.content_width)// 3, 0)
-        imgui.set_next_window_size(self.app.content_width // 3, (self.app.content_height * 3) // 4)
+        imgui.set_next_window_position((3 * self.app.content_width)// 4, 0)
+        imgui.set_next_window_size(self.app.content_width // 4, (self.app.content_height * 4) // 5)
         imgui.begin('GANSPACE##Menu', closable=False, flags=(imgui.WINDOW_NO_RESIZE | imgui.WINDOW_NO_MOVE))
         imgui.text("GANSPACE")
         self.pca()
         imgui.end()
 
-        imgui.set_next_window_position(0,(self.app.content_height * 3) // 4)
-        imgui.set_next_window_size(self.app.content_width, (self.app.content_height * 3) // 4)
+        imgui.set_next_window_position((3 * self.app.content_width)// 4, 0)
+        imgui.set_next_window_size(self.app.content_width // 3, (self.app.content_height * 4) // 5)
+        imgui.begin('Super-res##Menu', closable=False, flags=(imgui.WINDOW_NO_RESIZE | imgui.WINDOW_NO_MOVE))
+        imgui.text("Super-res")
+        self.super_res()
+        imgui.end()
+
+        imgui.set_next_window_position(0,(self.app.content_height * 4) // 5)
+        imgui.set_next_window_size(self.app.content_width, (self.app.content_height * 4) // 5)
         imgui.begin('Render##Menu', closable=False, flags=(imgui.WINDOW_NO_RESIZE | imgui.WINDOW_NO_MOVE))
         imgui.text("Renderer")
         if imgui_utils.button("START", width=self.app.button_w):
