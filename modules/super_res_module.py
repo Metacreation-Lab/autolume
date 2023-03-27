859d56dccf33c457767cc023251ab8bfd2c93ce6
petercmh01 <minghaipeterchan@outlook.com>
2023-03-27 22:11:48 +0000

    Replace super_res_module.py

--- modules/super_res_module.py
+++ modules/super_res_module.py
@@ -4,46 +4,61 @@ import imgui
 from utils.gui_utils import imgui_utils
 from super_res import main as super_res_main
 from super_res import base_args
+from dnnlib import EasyDict
 
-parser=base_args()
-args = parser.parse_args()
-scale_factor=[1,2,3,4,5,6,7,8]
+args = EasyDict(result_path="", input_path="", model_type="Balance",
+                outscale=4, width=4096, height=4096, sharpen_scale=4, fps=30)
+scale_factor = ['1', '2', '3', '4', '5', '6', '7', '8']
+
 
 class SuperResModule:
     def __init__(self, menu):
-        self.width=args.out_width
-        self.height=args.out_height
-        self.out_scale=args.outscale
-        self.sharpen=args.sharpen_scale
-        self.fps=args.fps
+        self.result_path=args.result_path
+        self.input_path = args.input_path
+        self.models = ['Quality','Balance','Fast']
+        self.model_selected = 0
+        self.model_type = self.models[self.model_selected]
+        self.width = args.width
+        self.height = args.height
+        self.out_scale = args.outscale
+        self.sharpen = args.sharpen_scale
         self.menu = menu
+        self.fps = args.fps
+
+        self.scale_mode = 0
 
     @imgui_utils.scoped_by_object_id
     def __call__(self):
-        imgui.text("Super_Resolution Module")
-
-        _, self.result_path = imgui.input_text("Save Path", self.save_path, 1024)
-        _, self.input_path = imgui.input_text("File Path", self.file_path, 1024)
-        _, self.model_path = imgui.input_text("Model Path", self.model_path, 1024)
-        _, self.model_type = imgui.input_text("Model Type", self.model_type, 1024)
+        _, self.input_path = imgui.input_text("Source Folder", self.input_path, 1024)
+        _, self.result_path = imgui.input_text("Destination Folder", self.result_path, 1024)
+        self.models = ['Quality','Balance','Fast']
+        if len(self.models) > 0:
+            _, self.model_selected = imgui.combo("Model", self.model_selected, self.models)
 
         _, self.fps = imgui.input_int("FPS", self.fps)
-        _, self.out_scale = imgui.combo("Out_scale", self.out_scale, scale_factor)
-        _, self.height = imgui.input_int("Height", self.height)
-        imgui.same_line()
-        _, self.width = imgui.input_int("Width", self.width)
-        _, self.sharpen = imgui.input_int("Sharpen", self.sharpen)
+        clicked, self.scale_mode = imgui.combo("Scale Mode", self.scale_mode, ["Custom", "Scale"])
+        if clicked:
+            print(self.scale_mode)
+        if self.scale_mode:
+            _, self.out_scale = imgui.combo("Scale Factor", self.out_scale, scale_factor)
+        else:
+            _, self.height = imgui.input_int("Height", self.height)
+            _, self.width = imgui.input_int("Width", self.width)
+        _, self.sharpen = imgui.input_int("Sharpening Factor", self.sharpen)
+        if imgui.is_item_hovered():
+            imgui.set_tooltip("Additional sharpening performed after super resolution")
+        try:
+            if imgui.button("Super Resolution"):
+                print("Super Resolution")
+                args.result_path = self.result_path
+                args.input_path = self.input_path
+                args.model_type = self.model_type
+                args.outscale = self.out_scale
+                args.out_height = self.height
+                args.out_width = self.width
+                args.sharpen_scale = self.sharpen
+                args.fps = self.fps
 
-        if imgui.button("Super Resolution"):
-            print("Super Resolution")
-            args.result_path=self.result_path
-            args.input_path=self.input_path
-            args.model_path=self.model_path
-            args.model_type=self.model_type
-            args.outscale=self.out_scale
-            args.out_height=self.height
-            args.out_width=self.width
-            args.sharpen_scale=self.sharpen
-            args.fps=self.fps
-            
-            super_res_main(args)
+                super_res_main(args)
+        except Exception as e:
+            print(e)
