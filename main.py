8ad30edcc939d68d80a66a9e7ace57d81b9d0675
jkraasch <jkraasch@sfu.ca>
2023-05-01 09:26:23 -0700

    automatically deleting torch_extensionn

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import glob
+from pathlib import Path
+import shutil
 import multiprocessing
 from modules.autolume_live import Autolume
 import torch
@@ -7,6 +10,13 @@ def main():
 
     Optional PATH argument can be used specify which .pkl file to load.
     """
+    torch_extension_pattern = 'C:/Users/*/AppData/Local/torch_extensions'
+    
+    matches = sorted(glob.glob(torch_extension_pattern))
+    if len(matches):
+        print("Found torch extension at", matches)
+        shutil.rmtree(matches[-1])
+        print("Deleted", matches[-1])
 
     app = Autolume()
 
@@ -16,9 +26,9 @@ def main():
     app.close()
 
 if __name__ == "__main__":
-    # torch.backends.cudnn.benchmark = True
-    # torch.backends.cuda.matmul.allow_tf32 = True
-    # torch.backends.cudnn.allow_tf32 = True
-    # torch.set_grad_enabled(False)
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.backends.cudnn.allow_tf32 = True
+    torch.set_grad_enabled(False)
     multiprocessing.set_start_method("spawn", force=True)
     main()
