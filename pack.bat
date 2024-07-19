886884b8b430e8bc0b4b57c70aa4a354a12503d4
Wenhao Yu <--global>
2024-07-18 22:53:40 -0700

    Auto packing script

---
+++ pack.bat
@@ -0,0 +1,5 @@
+@echo off
+
+pyinstaller.exe main.spec -y && xcopy assets dist\main\assets /s /e /y /i && xcopy sr_models dist\main\sr_models /s /e /y /i && xcopy models dist\main\models /s /e /y /i&& echo packing finished
+
+pause
