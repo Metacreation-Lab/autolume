1bc5a89c0815fad8b6cc7f75452fd896a8ef13c5
Wenhao Yu <--global>
2024-08-07 15:12:20 -0700

    add resources folder

--- pack.bat
+++ pack.bat
@@ -1,6 +1,5 @@
 @echo off
 
-D:\Anaconda3\envs\autolume\Scripts\pyinstaller.exe main.spec -y && xcopy assets dist\main\assets /s /e /y /i && xcopy
-sr_models dist\main\sr_models /s /e /y /i && xcopy models dist\main\models /s /e /y /i&& echo packing finished
+D:\Anaconda3\envs\autolume\Scripts\pyinstaller.exe main.spec -y && xcopy assets dist\main\assets /s /e /y /i && xcopy sr_models dist\main\sr_models /s /e /y /i && xcopy models dist\main\models /s /e /y /i&& echo packing finished
 
 pause
