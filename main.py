321ab64b23eedf8406f3f00f678d91e47176d777
NONAMEEEEE12345 <nonameeeee11@gmail.com>
2024-10-15 00:49:32 -0700

    fixed multiprocess, added packing script, fixed super_res bugs

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def main():
     app.close()
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     torch.backends.cudnn.benchmark = True
     torch.backends.cuda.matmul.allow_tf32 = True
     torch.backends.cudnn.allow_tf32 = True
