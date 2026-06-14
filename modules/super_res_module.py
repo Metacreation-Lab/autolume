6fac3b669c1c212e7ecdfb237dd1823b60959278
Lionel <ucodia@live.com>
2026-06-14 21:26:09 +0000

    fix: run super-resolution in a subprocess to keep the ui responsive

--- modules/super_res_module.py
+++ modules/super_res_module.py
@@ -11,7 +11,7 @@ import numpy as np
 from utils import device_utils
 from utils.device_utils import get_device
 from utils.gui_utils import imgui_utils
-from super_res.super_res import main as super_res_main, load_model, get_resolution, check_width_height, get_audio, Reader, Writer
+from super_res.super_res import main as super_res_main, load_model, get_resolution, check_width_height, get_audio, Reader, Writer, run_super_res
 
 from dnnlib import EasyDict
 import multiprocessing as mp
@@ -48,6 +48,9 @@ class SuperResModule:
         self.running = False
         self.writer = None
         self.reader = None
+        self.queue = mp.Queue()
+        self.reply = mp.Queue()
+        self.sr_process = None
         self.files = []
         self.file_idx = 0
         self.super_res_idx = 0
@@ -65,8 +68,14 @@ class SuperResModule:
         imgui.begin("Super Resolution", False)
         imgui.text('Super Resolution...')
         imgui.text("Files: " + str(self.file_idx + 1) + "/" + str(len(self.files)))
-        imgui.text("Current File: " + self.files[self.file_idx])
-        imgui.text("Progress: " + "#"*int((self.super_res_idx/self.total_frames*10) + 1) + " " + str((self.super_res_idx+1)/self.total_frames*100) + "%")
+        if self.file_idx < len(self.files):
+            imgui.text("Current File: " + self.files[self.file_idx])
+        if self.total_frames > 0:
+            percent = (self.super_res_idx + 1) / self.total_frames * 100
+            bar = "#" * int(self.super_res_idx / self.total_frames * 10 + 1)
+            imgui.text("Progress: " + bar + " " + f"{percent:.1f}%")
+        else:
+            imgui.text("Progress: preparing...")
         # self.eta is in seconds so we convert it to hours minutes and seconds if not -1
         if self.eta != -1:
             hours = int(self.eta/3600)
@@ -75,11 +84,20 @@ class SuperResModule:
             imgui.text("ETA: " + str(hours) + "h " + str(minutes) + "m " + str(seconds) + "s")
         imgui.text(str(self.super_res_idx) + "/" + str(self.total_frames) + " frames")
         imgui.end()
-        self.perform_super_res()
 
 
     @imgui_utils.scoped_by_object_id
     def __call__(self):
+        if not self.reply.empty():
+            msg = self.reply.get()
+            while not self.reply.empty():
+                msg = self.reply.get()
+            self.file_idx, self.super_res_idx, self.total_frames, self.eta, done = msg
+            if done:
+                self.running = False
+                if self.sr_process is not None:
+                    self.sr_process.join()
+                    self.sr_process = None
         help_width = imgui.calc_text_size("(?)").x + 10
         button_width = self.app.button_w
         spacing = self.app.spacing
@@ -196,14 +214,6 @@ class SuperResModule:
 
     def start_super_res(self):
         self.start_time = time.time()
-        if self.model_type == "Quality":
-            model_path = "./sr_models/Quality.pth"
-        elif self.model_type == "Balance":
-            model_path = "./sr_models/Balance.pth"
-        elif self.model_type == "Fast":
-            model_path = "./sr_models/Fast.pt"
-
-        self.super_res_model = load_model(self.model_type, model_path)
         self.files = self.input_path
 
         if not os.path.exists(self.result_path):
@@ -211,235 +221,16 @@ class SuperResModule:
 
         self.file_idx = 0
         self.super_res_idx = 0
+        self.total_frames = -1
+        self.eta = -1
 
         if len(self.files) == 0:
             self.running = False
+            return
 
-
-    # def perform_super_res(self):
-    #     self.start_time = time.time()
-
-    #     # 确保只有当super_res_idx为0时，才开始处理新的文件
-    #     if self.super_res_idx == 0:
-    #         file = self.files[self.file_idx]
-    #         self.start_time = time.time()
-    #         head, tail = os.path.split(file)
-            
-    #         # 检查文件是否为图像或视频
-    #         if file.lower().endswith(('jpg', 'png', 'jpeg', 'bmp')):
-    #             data_transformer = transforms.Compose([transforms.ToTensor()])
-    #             image = cv2.imread(file)
-    #             input_height, input_width = image.shape[0], image.shape[1]
-    #             image = data_transformer(image).to('cuda')
-    #             input = torch.unsqueeze(image, 0)
-
-    #             # 处理图像
-    #             with torch.inference_mode():
-    #                 output = self.super_res_model(input)
-    #                 output = F.adjust_sharpness(output, self.args.sharpen_scale) * 255
-    #                 output = output[0].permute(1, 2, 0).cpu().numpy().astype(np.uint8)
-                    
-    #                 if self.args.scale_mode:
-    #                     if self.args.outscale != 4:
-    #                         output = cv2.resize(output, 
-    #                                             (int(input_width * self.args.outscale), 
-    #                                             int(input_height * self.args.outscale)), 
-    #                                             interpolation=cv2.INTER_LINEAR)
-    #                 else:
-    #                     output = cv2.resize(output, 
-    #                                         (int(self.args.out_width), 
-    #                                         int(self.args.out_height)), 
-    #                                         interpolation=cv2.INTER_LINEAR)
-
-    #                 path = os.path.join(self.args.result_path, 
-    #                                     tail[:-4] + f'_result_{self.args.model_type}_{int(input_width * self.args.outscale)}x{int(input_height * self.args.outscale)}_Sharpness{self.args.sharpen_scale}.jpg')
-    #                 cv2.imwrite(path, output)
-    #             self.file_idx += 1  # 递增file_idx
-
-    #         elif file.lower().endswith(('mp4', 'avi', 'mov')):
-    #             # 处理视频文件
-    #             audio = get_audio(file)
-    #             self.video = cv2.VideoCapture(file)
-    #             self.fps = self.video.get(cv2.CAP_PROP_FPS)
-    #             self.total_frames = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
-    #             self.video_width = int(self.video.get(cv2.CAP_PROP_FRAME_WIDTH))
-    #             self.video_height = int(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-    #             # 设置 video_save_path
-    #             if self.args.scale_mode:
-    #                 output_width = int(self.video_width * self.args.outscale)
-    #                 output_height = int(self.video_height * self.args.outscale)
-    #             else:
-    #                 output_width = int(self.args.out_width)
-    #                 output_height = int(self.args.out_height)
-
-    #             # 生成视频保存路径
-    #             self.video_save_path = os.path.join(self.args.result_path,
-    #                                                 tail[:-4] + f'_result_{self.args.model_type}_{output_width}x{output_height}_Sharpness{self.args.sharpen_scale}.mp4')
-
-    #             print(f"Saving video to {self.video_save_path}")
-
-
-    #             self.writer = Writer(self.args, audio, self.video_height, self.video_width, 
-    #                                 video_save_path=self.video_save_path, fps=self.fps)
-    #             self.reader = Reader(self.video_width, self.video_height, file)
-
-    #             # 处理视频的每一帧
-    #             if self.super_res_idx < self.total_frames:
-    #                 img = self.reader.get_frame()
-    #                 if img is not None:
-    #                     sr_input = torch.tensor(img).permute(2, 0, 1).unsqueeze(0).float().to('cuda') / 255
-    #                     with torch.inference_mode():
-    #                         sr_output = self.super_res_model(sr_input)
-    #                         sr_output = F.adjust_sharpness(sr_output, self.args.sharpen_scale) * 255
-    #                         sr_output = sr_output[0].permute(1, 2, 0).cpu().numpy().astype(np.uint8)
-
-    #                         # 缩放和保存
-    #                         if self.args.scale_mode:
-    #                             if self.args.outscale != 4:
-    #                                 sr_output = cv2.resize(sr_output, 
-    #                                                     (int(self.video_width * self.args.outscale), 
-    #                                                         int(self.video_height * self.args.outscale)), 
-    #                                                     interpolation=cv2.INTER_LINEAR)
-    #                         else:
-    #                             sr_output = cv2.resize(sr_output, 
-    #                                                 (int(self.args.out_width), 
-    #                                                     int(self.args.out_height)), 
-    #                                                 interpolation=cv2.INTER_LINEAR)
-
-    #                         self.writer.write_frame(sr_output)
-    #                         self.super_res_idx += 1
-    #             else:
-    #                 if self.writer is not None:
-    #                     self.writer.close()
-    #                 self.super_res_idx = 0
-    #                 self.file_idx += 1
-
-    #     torch.cuda.empty_cache()
-    #     gc.collect()
-
-    #     # 当所有文件处理完后，停止运行
-    #     if self.file_idx >= len(self.files):
-    #         self.running = False
-
-   
-
-
-    def perform_super_res(self):
-        self.start_time = time.time()
-
-        # 确保只有当super_res_idx为0时，才开始处理新的文��
-        if self.super_res_idx == 0:
-            file = self.files[self.file_idx]
-            self.start_time = time.time()
-            head, tail = os.path.split(file)
-            
-            # 检查文件是否为图像或视频
-            if file.lower().endswith(('jpg', 'png', 'jpeg', 'bmp')):
-                data_transformer = transforms.Compose([transforms.ToTensor()])
-                image = cv2.imread(file)
-                input_height, input_width = image.shape[0], image.shape[1]
-                image = data_transformer(image).to(get_device())
-                input = torch.unsqueeze(image, 0)
-
-                # 处理图像
-                with torch.inference_mode():
-                    output = self.super_res_model(input)
-                    output = F.adjust_sharpness(output, self.args.sharpen_scale) * 255
-                    output = output[0].permute(1, 2, 0).cpu().numpy().astype(np.uint8)
-                    
-                    if self.args.scale_mode:
-                        if self.args.outscale != 4:
-                            output = cv2.resize(output, 
-                                                (int(input_width * self.args.outscale), 
-                                                int(input_height * self.args.outscale)), 
-                                                interpolation=cv2.INTER_LINEAR)
-                    else:
-                        output = cv2.resize(output, 
-                                            (int(self.args.out_width), 
-                                            int(self.args.out_height)), 
-                                            interpolation=cv2.INTER_LINEAR)
-
-                    path = os.path.join(self.args.result_path, 
-                                        tail[:-4] + f'_result_{self.args.model_type}_{int(input_width * self.args.outscale)}x{int(input_height * self.args.outscale)}_Sharpness{self.args.sharpen_scale}.jpg')
-                    cv2.imwrite(path, output)
-
-                # 在图像处理后，递增 file_idx 和 super_res_idx
-                self.file_idx += 1
-                self.super_res_idx = 0
-
-            elif file.lower().endswith(('mp4', 'avi', 'mov')):
-                # 视频处理逻辑...
-                self.process_video(file)  # 调用视频处理方法
-                self.file_idx += 1
-
-        # 清理内存
-        device_utils.empty_cache()
-        gc.collect()
-
-        # 当所有文件处理完后停止运行
-        if self.file_idx >= len(self.files):
-            self.running = False
-    
-    def process_video(self, file):
-        """处理视频文件的逻辑"""
-        audio = get_audio(file)  # 获取音频信息
-        video = cv2.VideoCapture(file)
-        fps = video.get(cv2.CAP_PROP_FPS)
-        total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-        video_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-        video_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        head, tail = os.path.split(file)
-
-        # 设置视频保存路径
-        if self.args.scale_mode:
-            video_save_path = os.path.join(self.args.result_path, 
-                                        tail[:-4] + f'_result_{self.args.model_type}_{int(video_width * self.args.outscale)}x{int(video_height * self.args.outscale)}_Sharpness{self.args.sharpen_scale}.mp4')
-        else:
-            video_save_path = os.path.join(self.args.result_path, 
-                                        tail[:-4] + f'_result_{self.args.model_type}_{int(self.args.out_width)}x{int(self.args.out_height)}_Sharpness{self.args.sharpen_scale}.mp4')
-
-        print(f"Saving video to {video_save_path}")
-
-        # 初始化 Writer 和 Reader
-        writer = Writer(self.args, audio, video_height, video_width, 
-                        video_save_path=video_save_path, fps=fps)
-        reader = Reader(video_width, video_height, file)
-        super_res_idx = 0  # 初始化帧计数
-
-        # 处理每一帧
-        while super_res_idx < total_frames:
-            print(f"Processing frame {super_res_idx}/{total_frames}")
-            img = reader.get_frame()
-            if img is not None:
-                with torch.inference_mode():
-                    sr_input = torch.tensor(img).permute(2, 0, 1).unsqueeze(0).float().to(get_device()) / 255
-                    sr_output = self.super_res_model(sr_input)
-                    sr_output = F.adjust_sharpness(sr_output, self.args.sharpen_scale) * 255
-                    sr_output = sr_output[0].permute(1, 2, 0).cpu().numpy().astype(np.uint8)
-
-                    # 根据 scale_mode 进行缩放
-                    if self.args.scale_mode:
-                        sr_output = cv2.resize(sr_output, 
-                                            (int(video_width * self.args.outscale), 
-                                                int(video_height * self.args.outscale)), 
-                                            interpolation=cv2.INTER_LINEAR)
-                    else:
-                        sr_output = cv2.resize(sr_output, 
-                                            (int(self.args.out_width), 
-                                                int(self.args.out_height)), 
-                                            interpolation=cv2.INTER_LINEAR)
-
-                    print(f"Saving frame {super_res_idx} to {video_save_path}")
-                    writer.write_frame(sr_output)
-                    super_res_idx += 1  # 处理下一帧
-
-        # 完成后关闭 writer
-        writer.close()
-
-        # 清理内存
-        device_utils.empty_cache()
-        gc.collect()
-
-
-    
+        # Run upscaling in a separate process so it gets the GPU at full speed and the UI stays responsive
+        self.queue = mp.Queue()
+        self.reply = mp.Queue()
+        self.sr_process = mp.Process(target=run_super_res, args=(self.queue, self.reply), daemon=True)
+        self.sr_process.start()
+        self.queue.put(self.args)
